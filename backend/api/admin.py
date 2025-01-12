04f9d6fb53cb6a1843f61490196095b4dfe02d2d
AdrianSajdak <adrian.sajdak@gmail.com>
2025-01-12 12:45:03 +0100

    Poprawki

--- backend/api/admin.py
+++ backend/api/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import *
+
+
+
+admin.site.register(CustomUser)
+admin.site.register(MembershipPlan)
+admin.site.register(Membership)
+admin.site.register(GroupClass)
+admin.site.register(Trainer)
